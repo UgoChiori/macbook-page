5ee0275776fbd990cd94cb1b6555c51926ea744b
checkr <ugochiori@gmail.com>
2026-02-20 09:35:12 +0100

    first commit, new files and folders created

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="bg-pink-500 width-full h-screen flex items-center justify-center">
+      <h1 className="text-4xl text-white font-bold">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
